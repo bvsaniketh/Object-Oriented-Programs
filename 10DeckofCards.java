1ce9ad56cb68113aa55ae9fd1380a4730ce846bc
bvsaniketh <bvsaniketh95@gmail.com>
2017-08-21 16:52:12 +0530

    Final COMMIT

--- 10DeckofCards.java
+++ 10DeckofCards.java
@@ -1,80 +1,227 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
+class Deck
+{	
+	public Card[]deck;
+   
+	public Card[][] setOfCards;
+	Deck()
+	{
+		deck = new Card[52];
+	}
+
+	//initialize the cards
+	void initializeCards()
+	{
+		int k=0;
+		for(int suit=0;suit<4;suit++)
+		{
+			for(int rank=1;rank<=13;rank++)
+			{
+				deck[k] = new Card(suit, rank);
+				k++;
+			}
+		}
+	}
+
+	//swapping a random card with the selected card
+	void shuffleCards()
+	{
+		for(int i=0;i<52;i++)
+		{
+			int rand = (int)(Math.random()*(i+1));
+			Card tempCard = deck[i];
+			deck[i] = deck[rand];
+			deck[rand] = tempCard;
+		}
+	}
+
+	void distributeCards()
+	{
+		int k=0;
+		setOfCards = new Card[4][9];
+		for(int player=0;player<4;player++)
+		{
+			for(int cards=0;cards<9;cards++)
+			{
+				setOfCards[player][cards] = deck[k++];//distributing cards serially
+			}
+		}
+	}
+
+	void displayCards()
+	{
+		for(int player=0;player<4;player++)
+		{
+			System.out.println("\nPlayer "+ (player+1) +":  ");
+			for(int cards=0;cards<9;cards++)
+			{
+				Card card = setOfCards[player][cards];
+				System.out.println("         "+toStringRank(card.rank) + " of " + toStringSuit(card.suit));
+			}
+		}
+	}
+
+	String toStringSuit(int suit)
+	{
+		switch(suit)
+		{
+			case 0:
+			     return "Clubs";
+			case 1:
+			     return "Diamonds";
+			case 2:
+			     return "Hearts";
+			case 3:
+			     return "Spades";
+		}
+		return "";
+	}
+	String toStringRank(int rank)
+	{
+		switch(rank)
+		{
+			case 1:
+			     return "Ace";
+			case 2:
+			     return "2";
+			case 3:
+			     return "3";
+			case 4:
+			     return "4";
+			case 5:
+			     return "5";
+			case 6:
+			     return "6";
+			case 7:
+			     return "7";
+			case 8:
+			     return "8";
+			case 9:
+			     return "9";
+			case 10:
+			     return "10";
+			case 11:
+			     return "Jack";
+			case 12:
+			     return "Queen";
+			case 13:
+			     return "King";
+		}
+		return "";
+	}
+
+	//swap method for integers
+	public void swap(Card[] array, int i, int j) 
+	{
+		Card temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	Card[] sort(int row)
+	{
+		for(int i=0 ; i<(setOfCards[row].length - 1); i++)
+		 {
+		 	for(int j=0 ; j<(setOfCards[row].length - i - 1); j++)
+		 	{
+		 		if(setOfCards[row][j].rank>setOfCards[row][j+1].rank) /* For decreasing order use < */
+		 		{
+		 			swap(setOfCards[row], j, j+1);
+        		}																												
+        	 }
+        }
+        return setOfCards[row];
+	}
+}
+
+
+class Card
+{
+	protected int suit;
+	protected int rank;
+	protected Card next;
+	Card(int suit, int rank)
+	{
+		this.suit = suit;
+		this.rank = rank;
+		this.next = null;
+	}
+}
+
 
 class Player
 {
-	int id;
-	String crr[]=new String[9];
+	Card front, rear;
+	Player()
+	{
+		front = null;
+		rear = null;
+	}
+	
+	boolean isEmpty()
+	{
+		if(front ==  null)
+			return true;
+		return false;
+	}
 
-	// Player(int value,String arr[])
-	// {
-	// 	id=value;
-	// 	for(int i=0;i<crr.length;i++)
-	// 	String crr[i]=arr[i];
-	// }
+	void enqueue(Card newCard)
+	{
+		if(isEmpty())
+		{
+			front = newCard;
+		}
+		else
+		{
+			rear.next = newCard;
+		}	
+		rear = newCard;
+	}
 
+	void dequeue()
+	{
+		if(front == null){
+			rear = null;
+		}
+		if(front.next == null){front = null;return;}
+		front = front.next;
+	}
 }
-
 
 
 class DeckOfCards
 {
-
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
-	
-		// make the cards
-		Player p1=new Player();
-		p1.id=1;
-		Player p2=new Player();
-		p2.id=2;
-		Player p3=new Player();
-		p2.id=3;
-		Player p4=new Player();
-		p2.id=4;
-		String[] deck = new String[52]; // how many total
-		String[] suits = {"Heart", "Diamond", "Spade", "Club"};
-		String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-		String[][]arr=new String[10][10];
-		int l=0;
-		
-	
-		for(int i=0;i<deck.length;i++)
-		{
-			deck[i]=suits[i/13] + " "+ ranks[i%13];
-			//System.out.println(deck[i]);
-		}
+	    //program 9 logic		
+		Deck d1 = new Deck();
+		d1.initializeCards();
+		d1.shuffleCards();
+		System.out.println("\nAfter shuffling and distribution of Cards");
+		d1.distributeCards();
+		d1.displayCards();
 
-		for(int k=0;k<deck.length;k++)
-		{
-			int random=(int)(Math.random()*deck.length);
-			String temp=deck[k];
-			deck[k]=deck[random];
-			deck[random]=temp;
-			//System.out.println(deck[k]);
-		}
+		//Program 10 logic
+		System.out.println("\n\nAfter sorting and adding in Queue, the cards are as follows");
+		Card cardArr[];
+		Player player;
+		for(int i=0;i<4;i++){
+			System.out.println("\nPlayer "+(i+1)+": ");
+			cardArr = d1.sort(i);
 
-		for(int j=0;j<deck.length;j++)
-		{
-
-		}
-		int i=0;
-		for(i=0;i<4;i++)	
-		{
-
-			System.out.println("\nPlayer " + (i+1)+ " has \n");
-			for(int j=0;j<9;j++)
+			//enqueing and dequeing and printting the player cards using queue
+			player = new Player();
+			for(Card card: cardArr)
 			{
-				arr[i][j]=deck[l];
-				l++;
-				System.out.println(arr[i][j]);
+				player.enqueue(card);
 			}
-			System.out.println();
-		}
 
+			//System.out.println(d1.toStringRank(player.front.rank) + " of "+ d1.toStringSuit(player.front.suit));
+		
+			for(int cards=0;cards<9;cards++)
+			{
+				System.out.println("         "+d1.toStringRank(player.front.rank) + " of "+ d1.toStringSuit(player.front.suit));
+				player.dequeue();
+			}
+	  	}
 	}
-
-
 }
-
--- 12Clinic.java
+++ 12Clinic.java
@@ -1,23 +1,26 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 class Clinic
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws ParseException
 	{
 		Scanner sc =new Scanner(System.in);
 
 
 		Doctor d;
 		Doctor dd5;
-		//d.addDoctor();
 		Patient p;
+		Patient pp5;
+		Appointment appo=new Appointment();
 		Find f=new Find();
-		//p.addPatient();
 		ArrayList<Doctor>dd1=new ArrayList<Doctor>();
-		// dd1.add(d);
+		ArrayList<Doctor>dsec=new ArrayList<Doctor>();
 		ArrayList<Patient>pp1=new ArrayList<Patient>();
+		HashMap<Appointment,Integer>appoint=new HashMap<Appointment,Integer>();
+
 		// pp1.add(p);
 
 		// Iterator itr=dd1.iterator();
@@ -37,9 +40,11 @@ class Clinic
 			System.out.println("2) Add Patient");
 			System.out.println("3) Display all Doctors");
 			System.out.println("4) Display all Patients");
-			System.out.println("5) Exit");
-			System.out.println("6) Search Doctor by name,id");
-			System.out.println("7) Search Patient by name,id");
+			System.out.println("5) Search Doctor by Name,ID,Specialization");
+			System.out.println("6) Search Patient by Name,ID,Mobile Number,Age");
+			System.out.println("7) Take Appointment");
+			System.out.println("8) Print Appointment");
+			System.out.println("9) Exit");
 			choice=sc.nextInt();
 			switch(choice)
 			{
@@ -72,16 +77,16 @@ class Clinic
 						}
 						break;
 
-				case 5: System.exit(0);
-
-				case 6: int ch1;
+				case 5: int ch1;
+						String opt1;
 						do
 						{
 							System.out.println("Search-----------");
 							System.out.println("1.Search Doctor by ID");
 							System.out.println("2.Search Doctor by Name");
-							System.out.println("3.Search Doctor By Availability");
-							System.out.println("4.Exit");
+							System.out.println("3.Search Doctor by Specialization");
+							System.out.println("4.Search Doctor By Availability");
+							System.out.println("5.Exit");
 							ch1=sc.nextInt();
 
 							switch(ch1)
@@ -91,59 +96,128 @@ class Clinic
 									  // f=new Find();
 
 									   dd5=f.FindbyId(dd1,chd);
-									   System.out.println(dd5.toString());
+									   System.out.println(dd5 +"\n");
 									   break; //Check this because it is array list printing
 
 								case 2:System.out.println("Enter the Doctor's Name");
 									   String chdname=sc.next();
 									   //f=new Find();
 									   dd5=f.FindbyName(dd1,chdname);
-									   System.out.println(dd5.toString());
+									   System.out.println(dd5.toString()+"\n");
 									   break;
 
-								case 3:break;
+								case 3:System.out.println("Enter the Doctor's Specialization");
+									   String speci=sc.next();
+									   dsec=f.FindBySpe(dd1,speci);
+									   System.out.println(dsec);
+									   break; 
 
-								case 4:System.exit(0);
+								case 4:break;
+
+								case 5:System.exit(0);
 
 								default:System.out.println("Invalid choice");
 										break;
 							}
-						}while(ch1<5);
+							System.out.println("Do you want perform any other functionlity ");
+							opt1=sc.next();
+						}while(!opt1.equalsIgnoreCase("no"));
 
-				case 7:int ch2;
+				case 6:int ch2;
+					   String opt2;
 						do
 						{
+							
 							System.out.println("Search-----------");
 							System.out.println("1.Search Patient by ID");
 							System.out.println("2.Search Patient by Name");
-							System.out.println("3.Search Patient By Availability");
-							System.out.println("4.Exit");
+							System.out.println("3.Search Patient by Mobile Number");
+							System.out.println("4.Search Patient by Age");
+							System.out.println("5.Exit");
 							ch2=sc.nextInt();
 
 							switch(ch2)
 							{
 								case 1:System.out.println("Enter the Patient's ID");
 									   int chp=sc.nextInt();
-									  
-									   pp1=f.FindbyIdPa(pp1,chp);
-									   System.out.println(pp1);
+									   pp5=f.FindbyIdPa(pp1,chp);
+									   //pp1=f.FindbyIdPa(pp1,chp);
+									   System.out.println(pp5.toString()+"\n");
 									   break; //Check this because it is array list printing
 
 								case 2:System.out.println("Enter the Patient's Name");
 									   String chpname=sc.next();
-									   
-									   pp1=f.FindbyNamePa(pp1,chpname);
-									   System.out.println(pp1);
+									   pp5=f.FindbyNamePa(pp1,chpname);
+									   //pp1=f.FindbyNamePa(pp1,chpname);
+									   System.out.println(pp5.toString()+"\n");
 									   break;
 
-								case 3:break;
+								case 3:System.out.println("Enter the Patient's Mobile Number");
+									   long mob=sc.nextLong();
+									   pp5=f.FindbyMob(pp1,mob);
+									   System.out.println(pp5.toString());
+									   break;
 
-								case 4:System.exit(0);
+								case 4:System.out.println("Enter the Patient's Age");
+									   int age1=sc.nextInt();
+									   pp5=f.FindbyAge(pp1,age1);
+									   System.out.println(pp5.toString());
+									   break;
+
+								case 5:System.exit(0);
 
 								default:System.out.println("Invalid choice");
 										break;
+
+								
+
+								
 							}
-						}while(ch2<5);
+
+							System.out.println("Do you want perform any other functionlity ");
+							opt2=sc.next();
+						}while(!opt2.equalsIgnoreCase("no"));
+
+
+				case 7: System.out.println("Enter the Doctor ID");
+						int did=sc.nextInt();
+
+
+						Doctor doc=f.FindbyId(dd1,did);
+						if(doc!=null)
+						{	
+
+							System.out.println("Enter the Patient ID");
+							int pid=sc.nextInt();
+							Patient pat=f.FindbyIdPa(pp1,pid);
+							System.out.println(pat);
+							if(pat!=null)
+							{
+							System.out.println("Enter the date in the format of dd/mm/yyyy");
+							String d1=sc.next();
+							SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+							Date dateapp=format.parse(d1);
+							//System.out.println(format.format(dateapp));
+							appo.setAppointment(dateapp,doc,pat);
+							}
+							else
+							{
+								System.out.println("Invalid Patient ID");
+							}
+					
+					    }
+					    else
+					    {
+					    	System.out.println("Invalid Doctor ID");
+					    }
+						break;
+						
+
+				case 8: System.out.println("Printing all appointments");
+						appo.printall();
+						break;	
+
+				case 9: System.exit(0);
 
 				default: System.out.println("Invalid option selected");
 
@@ -162,11 +236,13 @@ class Doctor
 	String spec;
 	String ava;
 	Scanner sc =new Scanner(System.in);
-
+	HashMap<Date,Integer>dapp =new HashMap<Date,Integer>();
+	ArrayList<Integer>patient1=new ArrayList<Integer>();
+	
 
 	public String toString()
 		{
-			return "Doctor ID " + did +"\n"+"Doctor Name "+name+ "\n" + "Doctor Specialization "+ spec+"\n"+ "Doctor Availability " +ava ;
+			return "Doctor ID " + did +"\n"+"Doctor Name "+name+ "\n" + "Doctor Specialization "+ spec+"\n"+ "Doctor Availability " +ava +"\n" ;
 		}
 
 	void addDoctor()
@@ -187,12 +263,12 @@ class Doctor
 		ava=sc.next();
 
 
-		System.out.println("\n\nThe details of the Doctor are :");
-		System.out.println("ID: " + did);
-		System.out.println("Name: " + name);
-		System.out.println("Specialization: " + spec);
-		System.out.println("Availability: " +ava);
-		System.out.println();
+		// System.out.println("\n\nThe details of the Doctor are :");
+		// System.out.println("ID: " + did);
+		// System.out.println("Name: " + name);
+		// System.out.println("Specialization: " + spec);
+		// System.out.println("Availability: " +ava);
+		// System.out.println();
 		
 
 	}
@@ -211,7 +287,7 @@ class Patient
 
 	public String toString()
 	{
-		return "Patient ID " + pid +"\n"+"Patient Name "+name+ "\n" + "Patient Mobile Number "+ mno+"\n"+ "Patient Age " +age ;
+		return "Patient ID " + pid +"\n"+"Patient Name "+name+ "\n" + "Patient Mobile Number "+ mno+"\n"+ "Patient Age " +age + "\n" ;
 	}
 
 	void addPatient()
@@ -232,50 +308,111 @@ class Patient
 		age=sc.nextInt();
 
 
-		System.out.println("\n\nThe details of the Patient are :");
-		System.out.println("ID: " + pid);
-		System.out.println("Name: " + name);
-		System.out.println("Mobile Number: " + mno);
-		System.out.println("Age: " +age);
-		System.out.println();
+		// System.out.println("\n\nThe details of the Patient are :");
+		// System.out.println("ID: " + pid);
+		// System.out.println("Name: " + name);
+		// System.out.println("Mobile Number: " + mno);
+		// System.out.println("Age: " +age);
+		// System.out.println();
 	}
 }
 
+class Appointment
+{
+	Integer did1;
+	Date date1;
+	int appcount=0;
+	Integer pid1;
+	HashMap<Date,Doctor>app=new HashMap<Date,Doctor>();
+	//HashMap<Map,String>printmap=new HashMap<Map,String>();
+	// Appointment(int did,Date date)
+	// {
+	// 	did1=did;
+	// 	date1=date;
+	// }
+	SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+	void setAppointment(Date dateapp,Doctor doc,Patient pat)
+	{
+		
+		if(doc.dapp.containsKey(dateapp))
+		{
+			appcount=doc.dapp.get(dateapp);
+			if(appcount>=5)
+			{
+				System.out.println("Full for the doctor this day");
+
+			}
+			else
+			{
+				appcount++;
+				app.put(dateapp,doc);
+				doc.dapp.put(dateapp,appcount);
+				doc.patient1.add(pat.pid);
+				//printmap.put(app,pat.name);
+				System.out.println("Appointment set with " + doc.name + " "+ doc.did + " with specialization " +doc.spec + 
+					" on " +format.format(dateapp));
+			}
+		}
+		else
+		{
+			app.put(dateapp,doc);
+			doc.dapp.put(dateapp,1);
+			doc.patient1.add(pat.pid);
+			//printmap.put(app,pat.name);
+			System.out.println("Appointment set with " + doc.name + " "+ doc.did + " with specialization " +doc.spec + 
+					" on " +format.format(dateapp));
+		}
+
+
+
+	}
+	void printall()
+	{
+		
+		int i=0;
+		Set patmap=app.entrySet();
+		Iterator mapIter=patmap.iterator();
+		for(Object obj:app.entrySet())
+		{
+			Map.Entry<Date,Doctor>entry=(Map.Entry)obj;
+			Doctor d=entry.getValue();
+			System.out.println("Name of the Doctor : " + d.name);
+			System.out.println("\n With Patient" + d.patient1.get(i));
+			System.out.println(" at " + entry.getKey());
+			System.out.println("\n");
+		}
+		
+
+	}
+	public String toString()
+	{
+		return "Doctor ID " +did1 + " Date of Appointment " + date1 + "\n";
+	}	
+}
 class Find
 {
 	Doctor FindbyId(ArrayList<Doctor> dd1,int chd )
 	{
 		Doctor temp=null;
-		//ArrayList<Doctor>temp=new ArrayList<Doctor>();
 			for(Doctor f:dd1)
 			{
 				temp=f;
 				if(f.did==chd)
 				{
-					System.out.println("hiahda");
-					break;
-					// temp.add(f);
+					System.out.println("Found Doctor ID");
+					return temp;
+					
 				}
+				// else
+				// {
+				// 	System.out.println("Not found");
+				// }
 			}
 		
-		return temp;
+		return null;
 	}
 
 
-// ArrayList<Doctor> FindbyId(ArrayList<Doctor> dd1,int chd )
-// 	{
-// 		ArrayList<Doctor>temp=new ArrayList<Doctor>();
-// 			for(Doctor f:dd1)
-// 			{
-// 				if(f.did==chd)
-// 				{
-// 					System.out.println("hiahda");
-// 					temp.add(f);
-// 				}
-// 			}
-		
-// 		return temp;
-// 	}
 
 
 	Doctor FindbyName(ArrayList<Doctor> dd1,String chdname)
@@ -288,43 +425,121 @@ class Find
 				temp=f;
 				if((f.name).equals(chdname))
 				{
-					System.out.println("afhuaf");
-					break;//temp.add(f);
-				}
-			}
-		}
-		return temp; 
-	}
-	ArrayList<Patient> FindbyIdPa(ArrayList<Patient>pp1,int chp )
-	{
-		ArrayList<Patient> temp = new ArrayList<Patient>();
-		{
-			for(Patient p:pp1)
-			{
-				if(p.pid==chp)
-				{
-					temp.add(p);
-				}
-			}
-
-		}
-		return temp;
-	}
-	ArrayList<Patient> FindbyNamePa(ArrayList<Patient>pp1,String chpname)
-	{
-		ArrayList<Patient> temp = new ArrayList<Patient>();
-		{
-			for(Patient p:pp1)
-			{
-				if((p.name).equals(chpname))
-				{
-					temp.add(p);
+					System.out.println("Found Doctor Name");
+					return temp;//temp.add(f);
 				}
 				else
 				{
 					System.out.println("Not found");
 				}
 			}
+		}
+		return null;
+	}
+
+	ArrayList<Doctor> FindBySpe(ArrayList<Doctor>dd1,String speci)
+	{
+		ArrayList<Doctor>temp= new ArrayList<Doctor>();
+		{
+			for(Doctor f:dd1)
+			{
+				
+				if((f.spec).equals(speci))
+				{
+					System.out.println("Specialization found");
+					temp.add(f);
+				}
+			}
+		}
+		return temp;
+	}
+	Patient FindbyIdPa(ArrayList<Patient>pp1,int chp )
+	{
+		int flag=0;
+		Patient temp = null;
+		{
+			for(Patient p:pp1)
+			{	
+				flag=0;
+				temp=p;
+				if(p.pid==chp)
+				{
+					System.out.println("Found Patient ID");
+					return temp;
+					
+				}
+				else
+				{
+					flag=1;
+				}
+
+			}
+			if(flag==1)
+			{
+				System.out.println("Not Found");
+			}
+
+		}
+		return null;
+	}
+	Patient FindbyNamePa(ArrayList<Patient>pp1,String chpname)
+	{
+		Patient temp = null;
+		{
+			for(Patient p:pp1)
+			{	temp=p;
+				if((p.name).equals(chpname))
+				{
+					System.out.println("Found Patient Name");
+					break;
+				}
+				else
+				{
+					System.out.println("Not found");
+				}
+			}
+		}
+		return temp;
+	}
+	Patient FindbyMob(ArrayList<Patient>pp1,long mob )
+	{
+		Patient temp = null;
+		{
+			for(Patient p:pp1)
+			{
+				temp=p;
+				if(p.mno==mob)
+				{
+					System.out.println("Found Mobile Number of Patient");
+					break;
+				}
+				else
+				{
+					System.out.println("Not found");
+				}
+			}
+
+		}
+		return temp;
+	}
+	Patient FindbyAge(ArrayList<Patient>pp1,int age1 )
+	{
+		Patient temp = null;
+		{
+			for(Patient p:pp1)
+			{
+				temp=p;
+				if(p.age==age1)
+				{
+					System.out.println("Found Patient ID");
+					break;
+				}
+				else
+				{
+					System.out.println("Not found");
+				}
+			}
+
 		}
 		return temp;
 	}
